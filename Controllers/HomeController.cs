0001dae9b5f28c00aee66d8af6dd83ade1fa2922
Irakli Berdzenishvili <129960530+bugha123123@users.noreply.github.com>
2024-03-28 14:19:48 +0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+using Instagram_Clone.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Instagram_Clone.Controllers
+{
+    public class HomeController : Controller
+    {
+      
+
+       
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+
+         }
+}
